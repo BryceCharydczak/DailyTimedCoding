75247987e3e8ac354deeb9a74ebb66429b13be52
brycetheroman <bryceromancharydczak@gmail.com>
2018-02-22 18:32:08 +0000

    more boredom

---
+++ Algorithms/Diagonal Difference/DiagonalDifference.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int diagonalDifference(int[][] a) {
+        // Complete this function
+        int n = a.length;
+        int first = 0;
+        int second = 0;
+        for (int i = 0; i < n; i++){
+            first += a[i][i];
+            second += a[n - (i+1)][i];
+        }
+        int answer = Math.abs(first - second);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[][] a = new int[n][n];
+        for(int a_i = 0; a_i < n; a_i++){
+            for(int a_j = 0; a_j < n; a_j++){
+                a[a_i][a_j] = in.nextInt();
+            }
+        }
+        int result = diagonalDifference(a);
+        System.out.println(result);
+        in.close();
+    }
+}
+
